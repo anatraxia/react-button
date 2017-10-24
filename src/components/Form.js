2260c40efb92c8139d503b9ea969d1339a250023
Iskandar <iskandar@whei.io>
2017-10-25 00:23:27 +0800

    include props function from Form to child components

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,19 +4,52 @@ import SelectInput from './SelectInput'
 import SubmitButton from './SubmitButton'
 
 export class Form extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        model: this.props.model
+      }
+    }
 
     mapPropsModelValue = (key) => {
-      return this.props.model[key]
+      return this.state.model[key]
+    }
+
+    updateModelState = (key, value) => {
+      let newModel =this.state.model || {};
+      newModel[key] = value;
+
+      this.setState({
+        model: newModel,
+        result: ""
+      });
+    }
+
+    submitForm = () => {
+      this.setState({
+        result: "Submit Button Clicked"
+      })
+    }
+
+    validateForm = () => {
+
     }
 
     renderFormChildren = (props) => {
       return React.Children.map(props.children, child => {
-        if(child.type === FormField || child.type === SubmitButton){
+        if(child.type === FormField || child.type === SelectInput){
           return React.cloneElement(child, {
             label: child.props.source,
-            value: this.mapPropsModelValue(child.props.source)})
-        } else if (child.type === SelectInput) {
-          return React.cloneElement(child, {label: child.props.source, selected: this.mapPropsModelValue(child.props.source)})
+            value: this.mapPropsModelValue(child.props.source),
+            updateModelState: this.updateModelState
+          })
+        }  else if (child.type === SubmitButton) {
+          return React.cloneElement(child, {
+            label: child.props.source,
+            value: this.mapPropsModelValue(child.props.source),
+            validateForm: this.validateForm,
+            submitForm: this.submitForm
+          })
         } else {
           return child
         }
@@ -29,6 +62,7 @@ export class Form extends Component {
             <h1>{this.props.title}</h1>
             {this.renderFormChildren(this.props)}
           </form>
+          <p id="Result">{this.state.result}</p>
         </div>
       )
     }
--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
 export class SubmitButton extends Component {
-
+  onClick = () => {
+      console.log("Clicked Form")
+      this.props.submitForm()
+  }
   render () {
     return (
       <div>
-        <input type={this.props.label} value={this.props.value}/>
+        <input type={this.props.label} value={this.props.value} onClick={this.onClick}/>
       </div>
     )
   }
