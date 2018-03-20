c1506d7d825d278bed031eca6ced1348a0fdc387
Iskandar <anatraxia@Iskandars-MacBook-Pro.local>
2018-03-20 10:27:18 +0800

    add HOC into the storybook stories

--- src/components/addHeader.js
+++ src/components/addHeader.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import Form from './Form'
 
-function addHeader(Form) {
+function addHeader(WrappedComponent) {
   class AddHeader extends React.Component {
     render() {
       return (
         <div>
-        <header>
           <h1>New Header Added</h1>
-          </header>
-        <Form></Form>
+          <WrappedComponent {...this.props}/>
         </div>
       );
     }
--- src/components/stories/hocDemo.js
+++ src/components/stories/hocDemo.js
@@ -4,9 +4,25 @@ import Form from '../Form';
 import FormField from '../FormField';
 import SelectInput from '../SelectInput';
 import SubmitButton from '../SubmitButton';
-import AddHeader from '../addHeader';
+import addHeader from '../addHeader';
+
+const AddHeader = addHeader(Form);
+
+var model = {
+  'email': 'chris@shiip.io',
+  'name_first': 'Chris',
+  'name_last': 'Cai',
+  'status': 'active',
+  'submit': 'Submit Form'
+}
 
 storiesOf('Addheader', module)
   .add('test HOC component', () => (
-    <AddHeader></AddHeader>
+    <div>
+    <AddHeader model={model} title="New Title">
+     <FormField source='email' />
+     <FormField source='name_first' />
+     <FormField source='name_last' />
+     </AddHeader>
+    </div>
   ));
